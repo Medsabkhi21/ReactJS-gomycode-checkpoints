86ff49c075a8022510b81304d3abe97e18b2eb96
sabkhi <58121416+Medsabkhi21@users.noreply.github.com>
2022-06-08 20:11:57 +0100

    last

---
+++ Sabkhi's shop/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ Sabkhi's shop/src/containers/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      
+        <h2>Sabkhi's Shop</h2>
+      
+    </div>
+  );
+};
+
+export default Header;
